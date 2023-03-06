fa2c47380024bb4712eeb611421f059d6abaff27
Pavel <pavlozherebnov@gmail.com>
2023-03-06 20:08:17 +0200

    Modified CORS

--- src/components/utils/axios/index.ts
+++ src/components/utils/axios/index.ts
@@ -1,16 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const instance = axios.create({
-    baseURL: 'http://localhost:4000',
-    timeout: 1000,
-    headers: {'X-Custom-Header': 'foobar'}
-  });
+  baseURL: 'http://192.168.1.4:4000/',
+  timeout: 1000,
+  headers: { 'X-Custom-Header': 'foobar' },
+});
 
-  export const coingeckoapi = axios.create({
-    baseURL: 'https://api.coingecko.com/api/v3',
-    timeout: 1000,
-    headers: {'X-Custom-Header': 'foobar'}
-  });
-
-
-
+export const coingeckoapi = axios.create({
+  baseURL: 'https://api.coingecko.com/api/v3',
+  timeout: 1000,
+  headers: { 'X-Custom-Header': 'foobar' },
+});
