c45f1a05de1fec9b39dcc3c68267e9aecd0ad797
Pavel <pavlozherebnov@gmail.com>
2023-03-07 14:16:59 +0200

    modified instance for crossplatform servering

--- src/components/utils/axios/index.ts
+++ src/components/utils/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: 'http://192.168.1.4:4000/',
+  baseURL: 'http://192.168.1.11:4000/',
   timeout: 1000,
   headers: { 'X-Custom-Header': 'foobar' },
 });
