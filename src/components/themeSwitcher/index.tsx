1cd6aba805c95ee66afed6cbe2e6df0c776c78b2
Pavel <pavlozherebnov@gmail.com>
2023-03-14 00:48:44 +0200

    data fetch bug has fixed

--- src/components/themeSwitcher/index.tsx
+++ src/components/themeSwitcher/index.tsx
@@ -1,31 +1,30 @@
-import { Grid, IconButton,useTheme } from '@mui/material'
-import React, { FC, useContext } from 'react'
+import { Grid, IconButton, useTheme } from '@mui/material';
+import React, { FC, useContext } from 'react';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import { ColorModeContext } from '../../theme';
 import { useStyles } from './styles';
 
-
-export const ThemeSwitcherAndNotificationsComponent:FC = ():JSX.Element => {
-    const colorMode: any = useContext(ColorModeContext);
-    const theme = useTheme();
-    const classes = useStyles();
-  return (
-    <Grid
-                onClick={colorMode.toggleColorMode}
-                className={classes.iconBlock}
-              >
-                <IconButton className={classes.themeIcon}>
-                  {theme.palette.mode === 'dark' ? (
-                    <LightModeIcon />
-                  ) : (
-                    <DarkModeIcon />
-                  )}
-                </IconButton>
-                <IconButton>
-                  <NotificationsNoneIcon />
-                </IconButton>
-              </Grid>
-  )
-}
+export const ThemeSwitcherAndNotificationsComponent: FC = (): JSX.Element => {
+   const colorMode: any = useContext(ColorModeContext);
+   const theme = useTheme();
+   const classes = useStyles();
+   return (
+      <Grid className={classes.iconBlock}>
+         <IconButton
+            onClick={colorMode.toggleColorMode}
+            className={classes.themeIcon}
+         >
+            {theme.palette.mode === 'dark' ? (
+               <LightModeIcon />
+            ) : (
+               <DarkModeIcon />
+            )}
+         </IconButton>
+         <IconButton>
+            <NotificationsNoneIcon />
+         </IconButton>
+      </Grid>
+   );
+};
